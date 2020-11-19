97663a679d7fd27ab726394d93bb71c337c67200
koftov <ahronlu@gmail.com>
2020-11-19 12:01:02 +0200

    push to github and to heroku

---
+++ controllers/posts.js
@@ -0,0 +1,83 @@
+import mongoose from "mongoose";
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+
+    res.json(postMessages);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+
+    await res.status(201).json(newPost);
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send("No post with that id");
+  }
+
+  try {
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+      new: true,
+    });
+
+    res.json(updatedPost);
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send("No post with that id");
+  }
+  try {
+    await PostMessage.findByIdAndDelete(id);
+
+    res.json({ message: "Post deleted successfully" });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send("No post with that id");
+  }
+
+  try {
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(
+      id,
+      { likeCount: post.likeCount + 1 },
+      {
+        new: true,
+      }
+    );
+
+    res.json(updatedPost);
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
