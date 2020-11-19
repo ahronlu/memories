ccd904053a08e1cbb0bce3e73b3c16e17f9e9670
koftov <ahronlu@gmail.com>
2020-11-19 12:11:32 +0200

    deploy to heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -15,6 +16,14 @@ app.use(cors());
 app.use("/posts", postRoutes);
 
 const PORT = process.env.PORT;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 mongoose
   .connect(process.env.MONGO_URL, {
